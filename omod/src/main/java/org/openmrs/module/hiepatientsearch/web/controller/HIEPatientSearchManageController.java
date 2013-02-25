90ffd364bd2e583e75bbe228097dff93f69ee47a
suranga <surangakas@gmail.com>
2013-02-25 14:10:39 +0530

    Initial commit

---
+++ omod/src/main/java/org/openmrs/module/hiepatientsearch/web/controller/HIEPatientSearchManageController.java
@@ -0,0 +1,339 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.hiepatientsearch.web.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.PersonAddress;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.addresshierarchyrwanda.AddressHierarchy;
+import org.openmrs.module.addresshierarchyrwanda.AddressHierarchyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * The main controller.
+ */
+@Controller
+public class HIEPatientSearchManageController {
+
+	protected final Log log = LogFactory.getLog(getClass());
+
+	@RequestMapping(value = "/findPatient.htm", method = RequestMethod.GET)
+	public void manage(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		System.out.println("Test ! triggered by the patient search action");
+
+		response.sendRedirect(request.getContextPath()
+				+ "/module/hiepatientsearch/manage.form");
+	}
+
+	@RequestMapping(value = "/module/hiepatientsearch/manage", method = RequestMethod.GET)
+	public String manager(ModelMap model, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		System.out.println("Test ! triggered by the patient search action");
+		model.addAttribute("user", Context.getAuthenticatedUser());
+		
+		model = populateModel(model);
+		return "/module/hiepatientsearch/manage";
+	}
+			
+	private ModelMap populateModel(ModelMap model) {
+		AddressHierarchyService service = (AddressHierarchyService) Context
+				.getService(AddressHierarchyService.class);
+
+		
+		List<AddressHierarchy> stateProvince = new ArrayList<AddressHierarchy>();
+		List<String> stateProvinceString = new ArrayList<String>();
+		stateProvinceString.add("select province");
+		List<AddressHierarchy> countryDistrict = new ArrayList<AddressHierarchy>();
+		List<String> countryDistrictString = new ArrayList<String>();
+		countryDistrictString.add("select district");
+		List<AddressHierarchy> sector = new ArrayList<AddressHierarchy>();
+		List<String> sectorString = new ArrayList<String>();
+		sectorString.add("select sector");
+		List<AddressHierarchy> cell = new ArrayList<AddressHierarchy>();
+		List<String> cellString = new ArrayList<String>();
+		cellString.add("select cell");
+		List<AddressHierarchy> village = new ArrayList<AddressHierarchy>();
+		List<String> villageString = new ArrayList<String>();
+		villageString.add("select village");
+		
+		stateProvince = service.searchHierarchy("%", 2);
+		countryDistrict = service.searchHierarchy("%", 3);
+		sector = service.searchHierarchy("%", 4);
+		cell = service.searchHierarchy("%", 5);
+		village = service.searchHierarchy("%", 6);
+
+		for(AddressHierarchy ah : stateProvince) {
+			stateProvinceString.add(ah.getLocationName());
+		}
+		
+		for(AddressHierarchy ah : countryDistrict) {
+			countryDistrictString.add(ah.getLocationName());
+		}
+		
+		for(AddressHierarchy ah : sector) {
+			sectorString.add(ah.getLocationName());
+		}
+		
+		for(AddressHierarchy ah : cell) {
+			cellString.add(ah.getLocationName());
+		}
+		
+		for(AddressHierarchy ah : village) {
+			villageString.add(ah.getLocationName());
+		}
+		
+		
+		model.addAttribute("stateProvince", stateProvinceString);
+		model.addAttribute("countryDistrict", countryDistrictString);
+		model.addAttribute("sector", sectorString);
+		model.addAttribute("cell", cellString);
+		model.addAttribute("village", villageString);
+		
+		return model;
+	}
+
+	@RequestMapping(value = "/module/hiepatientsearch/manage", method = RequestMethod.POST)
+	public String advancedSearch(@RequestParam(value="patientIds") String patientIds,
+			@RequestParam("stateProvince") String stateProvince,
+			@RequestParam("countryDistrict") String countryDistrict,
+			@RequestParam("sector") String sector,
+			@RequestParam("cell") String cell,
+			@RequestParam("village") String village,
+			@RequestParam("moreThanAge") String moreThanAge,
+			@RequestParam("lessThanAge") String lessThanAge,
+			@RequestParam("date") Date dateString,
+			@RequestParam(value="gender") String gender, ModelMap model) {
+		String message = "Patient Id's" + patientIds + gender;
+		System.out.println(stateProvince);
+		System.out.println(countryDistrict);
+
+		String[] patientIdList = patientIds.split(",");
+
+		System.out.println(message);
+
+		List<Patient> patients = new ArrayList<Patient>();
+		List<Patient> patientLists = new ArrayList<Patient>();
+
+
+		for (int i = 0; i < patientIdList.length; i++) {
+			patients = Context.getPatientService().getPatients(null,
+					patientIdList[i], null, false);
+			System.out.println("size is " + patients);
+			System.out.println("gender " + patients.get(0).getGender());
+			patientLists.add(patients.get(0));
+		}
+
+		System.out.println("Total size " + patientLists.size());
+
+		if (gender != null) {
+			patientLists = genderFilter(patientLists, gender);
+		}
+
+		if (moreThanAge != "" || lessThanAge != "") {
+			patientLists = ageFilter(patientLists, moreThanAge, lessThanAge);
+		}
+
+		if (dateString != null) {
+			Date date = null;
+			// patientLists = dobFilter(patientLists, date);
+		}
+		
+		if (stateProvince != null && stateProvince != ""){
+			patientLists = stateProvinceFilter(patientLists, stateProvince);
+		}
+		
+		if (countryDistrict != null && countryDistrict != ""){
+			patientLists = countryDistrictFilter(patientLists, countryDistrict);
+		}
+		
+		if (sector != null && sector != ""){
+			patientLists = sectorFilter(patientLists, sector);
+		}
+		
+		if (cell != null && cell != ""){
+			patientLists = cellFilter(patientLists, cell);
+		}
+		
+		if (village != null && village != ""){
+			patientLists = villageFilter(patientLists, village);
+		}
+
+		System.out.println("tesy final size = " + patientLists.size());
+		patientLists.add(Context.getPatientService().getPatient(2));
+		model.addAttribute("patientLists", patientLists);
+		
+		model = populateModel(model);
+		
+		return "/module/hiepatientsearch/manage";
+	}
+
+	private List<Patient> villageFilter(List<Patient> patientLists,
+			String village) {
+		Iterator<Patient> iterator = patientLists.iterator();
+		while (iterator.hasNext()) {
+			Patient patient = iterator.next();
+			PersonAddress pa = patient.getPersonAddress();
+			if (!pa.getAddress1().equals(village)){
+				iterator.remove();
+			}
+		}
+	return patientLists;
+	}
+
+	private List<Patient> cellFilter(List<Patient> patientLists, String cell) {
+		Iterator<Patient> iterator = patientLists.iterator();
+		while (iterator.hasNext()) {
+			Patient patient = iterator.next();
+			PersonAddress pa = patient.getPersonAddress();
+			if (!pa.getAddress3().equals(cell)){
+				iterator.remove();
+			}
+		}
+	return patientLists;
+	}
+
+	private List<Patient> sectorFilter(List<Patient> patientLists, String sector) {
+		Iterator<Patient> iterator = patientLists.iterator();
+		while (iterator.hasNext()) {
+			Patient patient = iterator.next();
+			PersonAddress pa = patient.getPersonAddress();
+			if (!pa.getCityVillage().equals(sector)){
+				iterator.remove();
+			}
+		}
+	return patientLists;
+	}
+
+	private List<Patient> countryDistrictFilter(List<Patient> patientLists,
+			String countryDistrict) {
+		Iterator<Patient> iterator = patientLists.iterator();
+		while (iterator.hasNext()) {
+			Patient patient = iterator.next();
+			PersonAddress pa = patient.getPersonAddress();
+			if (!pa.getCountyDistrict().equals(countryDistrict)){
+				iterator.remove();
+			}
+		}
+	return patientLists;
+	}
+
+	private List<Patient> stateProvinceFilter(List<Patient> patientLists,
+			String stateProvince) {
+			Iterator<Patient> iterator = patientLists.iterator();
+			while (iterator.hasNext()) {
+				Patient patient = iterator.next();
+				PersonAddress pa = patient.getPersonAddress();
+				if (!pa.getStateProvince().equals(stateProvince)){
+					iterator.remove();
+				}
+			}
+		return patientLists;
+	}
+
+	private List<Patient> ageFilter(List<Patient> patientLists,
+			String moreThanAge, String lessThanAge) {
+		if (moreThanAge != null && lessThanAge == null) {
+			Iterator<Patient> iterator = patientLists.iterator();
+			while (iterator.hasNext()) {
+				if (iterator.next().getAge() > Integer.parseInt(moreThanAge)) {
+
+				} else {
+					System.out.println("equals");
+					iterator.remove();
+					System.out.println("removed !");
+				}
+			}
+
+		} else if (moreThanAge == null && lessThanAge != null) {
+			Iterator<Patient> iterator = patientLists.iterator();
+			while (iterator.hasNext()) {
+				if (iterator.next().getAge() < Integer.parseInt(lessThanAge)) {
+
+				} else {
+					System.out.println("equals");
+					iterator.remove();
+					System.out.println("removed !");
+				}
+			}
+
+		} else if (moreThanAge != null && lessThanAge != null) {
+			Iterator<Patient> iterator = patientLists.iterator();
+			while (iterator.hasNext()) {
+				int age = iterator.next().getAge();
+				if (age > Integer.parseInt(moreThanAge)
+						&& age < Integer.parseInt(lessThanAge)) {
+
+				} else {
+					System.out.println("equals");
+					iterator.remove();
+					System.out.println("removed !");
+				}
+			}
+
+		}
+		return patientLists;
+	}
+
+	
+	   @RequestMapping(value = "/redirect", method = RequestMethod.GET)
+	   public String redirect() {
+	     
+	      return "manage";
+	   }
+	   
+	   @RequestMapping(value = "/manage", method = RequestMethod.GET)
+	   public String finalPage() {
+	     
+	      return "manage";
+	   }
+	   
+	private void dobFilter(List<Patient> patientLists, Date date) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private List<Patient> genderFilter(List<Patient> patientLists, String gender) {
+		Iterator<Patient> iterator = patientLists.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().getGender().equals(gender)) {
+				System.out.println("equals");
+				iterator.remove();
+				System.out.println("removed !");
+			}
+		}
+		System.out.println("D" + patientLists.size());
+		return patientLists;
+	}
+
+}
---
+++ omod/target/hiepatientsearch-1.0-SNAPSHOT/web/module/manage.jsp
@@ -0,0 +1,212 @@
+<%@ include file="/WEB-INF/template/include.jsp"%>
+<%@ include file="/WEB-INF/template/header.jsp"%>
+<openmrs:require privilege="View Patients" otherwise="/login.htm"
+	redirect="/index.htm" />
+<style>
+#openmrsSearchTable_wrapper {
+	/* Removes the empty space between the widget and the Create New Patient section if the table is short */
+	/* Over ride the value set by datatables */
+	min-height: 0px;
+	height: auto !important;
+}
+</style>
+
+<script language="javascript">
+function advancedSearch()
+{
+	var table = document.getElementById("openmrsSearchTable");
+	var val ;
+	var idString = '';
+		
+	for (var i = 1, row; row = table.rows[i]; i++) {
+		   //iterate through rows
+		   //rows would be accessed using the "row" variable assigned in the for loop
+			  val = row.cells[0].innerHTML;
+		   	  idString = idString + val +"," ;
+		     //columns would be accessed using the "col" variable assigned in the for loop 
+		}
+	
+	document.getElementById('patientIds').value = idString ; 
+	
+	if(document.getElementById('province').value != "select province"){
+	document.getElementById('stateProvince').value = document.getElementById('province').value;
+	}else {
+		document.getElementById('stateProvince').value = " ";
+	}
+	
+	if(document.getElementById('district').value != "select district"){
+	document.getElementById('countryDistrict').value = document.getElementById('district').value;
+	} else {
+		document.getElementById('countryDistrict').value = " ";
+	}
+	
+	if(document.getElementById('sectors').value != "select sector"){
+	document.getElementById('sector').value = document.getElementById('sectors').value;
+	}else{
+		document.getElementById('sector').value = " ";
+	}
+	
+	if(document.getElementById('cells').value != "select cell"){
+	document.getElementById('cell').value = document.getElementById('cells').value;
+	}else {
+		
+	}
+	
+	if(document.getElementById('villages').value != "select village"){
+	document.getElementById('village').value = document.getElementById('villages').value;
+	}else {
+		document.getElementById('village').value = " ";
+	}
+	
+}
+</script>
+
+<openmrs:htmlInclude file="/dwr/interface/DWRPatientService.js" />
+<openmrs:htmlInclude
+	file="/scripts/jquery/dataTables/css/dataTables_jui.css" />
+<openmrs:htmlInclude
+	file="/scripts/jquery/dataTables/js/jquery.dataTables.min.js" />
+<openmrs:htmlInclude file="/scripts/jquery-ui/js/openmrsSearch.js" />
+
+<openmrs:globalProperty key="patient.listingAttributeTypes"
+	var="attributesToList" />
+
+<script type="text/javascript">
+				var lastSearch;
+				$j(document).ready(function() {
+					new OpenmrsSearch("findPatients", false, doPatientSearch, doSelectionHandler,
+						[	{fieldName:"identifier", header:omsgs.identifier},
+							{fieldName:"givenName", header:omsgs.givenName},
+							{fieldName:"middleName", header:omsgs.middleName},
+							{fieldName:"familyName", header:omsgs.familyName},
+							{fieldName:"age", header:omsgs.age},
+							{fieldName:"gender", header:omsgs.gender},
+							{fieldName:"birthdateString", header:omsgs.birthdate},
+							{fieldName:"deathDateString", header:omsgs.deathdate}
+						],
+						{
+                            searchLabel: '<spring:message code="Patient.searchBox" javaScriptEscape="true"/>',
+                            searchPlaceholder:'<spring:message code="Patient.searchBox.placeholder" javaScriptEscape="true"/>',
+                            attributes: [
+                                     	<c:forEach var="attribute" items="${fn:split(attributesToList, ',')}" varStatus="varStatus">
+                                           <c:if test="${fn:trim(attribute) != ''}">
+                                           <c:set var="attributeName" value="${fn:trim(attribute)}" />
+                                			     <c:choose>
+                                					<c:when test="${varStatus.index == 0}">
+                                						{name:"${attributeName}", header:"<spring:message code="PersonAttributeType.${fn:replace(attributeName, ' ', '')}" text="${attributeName}"/>"}
+                                					</c:when>
+                                					<c:otherwise>
+                                						,{name:"${attributeName}", header:"<spring:message code="PersonAttributeType.${fn:replace(attributeName, ' ', '')}" text="${attributeName}"/>"}
+                                					</c:otherwise>
+                                				 </c:choose>
+                                           </c:if>
+                                   		</c:forEach>
+                                     ]
+                            <c:if test="${not empty param.phrase}">
+                                , searchPhrase: '<spring:message text="${ param.phrase }" javaScriptEscape="true"/>'
+                            </c:if>                      
+                        });
+
+					//set the focus to the first input box on the page(in this case the text box for the search widget)
+					var inputs = document.getElementsByTagName("input");
+				    if(inputs[0])
+				    	inputs[0].focus();
+
+
+				});
+
+				function doSelectionHandler(index, data) {
+					document.location = "${model.postURL}?patientId=" + data.patientId + "&phrase=" + lastSearch;
+				}
+
+				//searchHandler for the Search widget
+				function doPatientSearch(text, resultHandler, getMatchCount, opts) {
+					lastSearch = text;
+					DWRPatientService.findCountAndPatients(text, opts.start, opts.length, getMatchCount, resultHandler);
+				}
+
+			</script>
+
+<div>
+	<b class="boxHeader"><spring:message code="Patient.find" /></b>
+	<div class="box">
+		<div class="searchWidgetContainer" id="findPatients"></div>
+	</div>
+</div>
+<br />
+<div>
+	<form name="input" method="POST">
+		<fieldset>
+			<legend>
+				<b>Optional Search parameters</b>
+			</legend>
+			<br /> Age between <input type="text" name="moreThanAge"> and
+			<input type="text" name="lessThanAge"> <br /> DOB :
+			<openmrs_tag:dateField formFieldName="date" startValue="" />
+			<br /> Gender :<input type="radio" name="gender" id="gender"
+				checked="checked" value="M">Male <input type="radio"
+				name="gender" id="gender" value="F">Female<br /> Province:
+
+			<%-- 		<form:select path="province">
+					  <form:option value="NONE" label="--- Select ---" />
+					  <form:options items="${stateProvince}" />
+				       </form:select> --%>
+
+
+			Province:
+			<form:select path="stateProvince" id="province"
+				items="${stateProvince}" />
+			<br /> District:
+			<form:select path="countryDistrict" id="district"
+				items="${countryDistrict}" />
+			<br /> Sector:
+			<form:select path="sector" id="sectors" items="${sector}" />
+			<br /> Cell:
+			<form:select path="cell" id="cells" items="${cell}" />
+			<br /> Village:
+			<form:select path="village" id="villages" items="${village}" />
+			<br /> <input type="hidden" id="patientIds" name="patientIds">
+			<input type="hidden" id="stateProvince" name="stateProvince">
+			<input type="hidden" id="countryDistrict" name="countryDistrict">
+			<input type="hidden" id="sector" name="sector"> 
+			<input type="hidden" id="cell" name="cell">
+			<input type="hidden" id="village" name="village"> 
+			<tab><tab><tab><input type="submit" value="Submit" label="Search" onclick="advancedSearch()">
+		</fieldset>
+	</form>
+
+<%-- <c:if test="${not empty patientLists}">
+	<c:forEach var="patient" items='$(patientLists}'>
+		${patient}
+	</c:forEach>
+	</c:if> --%>
+	
+	<c:if test="${not empty patientLists}">
+	<table border="1">
+	<tr><td>Identifer</td><td>Given Name</td></tr>
+		<c:forEach var="patient" items="${patientLists}">
+		<tr><td>
+			${patient.patientId}
+			</td>
+			<td>
+			${patient.givenName}
+			</td>
+			</tr>
+	</c:forEach>
+	</table>
+	</c:if>
+
+</div>
+
+<c:if test="${empty model.hideAddNewPatient}">
+	<openmrs:hasPrivilege privilege="Add Patients">
+		<br /> &nbsp; <spring:message code="general.or" />
+		<br />
+		<br />
+		<openmrs:portlet id="addPersonForm" url="addPersonForm"
+			parameters="personType=patient|postURL=admin/person/addPerson.htm|viewType=${model.viewType}" />
+	</openmrs:hasPrivilege>
+</c:if>
+
+
+<%@ include file="/WEB-INF/template/footer.jsp"%>
